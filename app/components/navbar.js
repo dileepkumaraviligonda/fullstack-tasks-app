6009206d40c90453dfe8285e9bc0a0c78f56ae2b
Dileep Kumar <dilludillu220@gmail.com>
2026-04-14 18:39:07 +0530

    add navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#222", color: "white" }}>
-      <h2>My Website</h2>
-
-      <div style={{ display: "flex", gap: "10px" }}>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-      </div>
-    </nav>
+    <div style={{ padding: 10, background: "#eee" }}>
+      <h3>My App 🚀</h3>
+    </div>
   );
 }
