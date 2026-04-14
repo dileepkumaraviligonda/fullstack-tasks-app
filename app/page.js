7c18bccabdc948a9dccea7d970f43e08101fe163
Dileep Kumar <dilludillu220@gmail.com>
2026-04-14 18:37:45 +0530

    fix navbar error

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>Welcome to my Next.js app 🚀</p>
     </div>
