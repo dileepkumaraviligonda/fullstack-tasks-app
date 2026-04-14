5f8ca9e03e224436f7784aba0595448b3bc3918f
Dileep Kumar <dilludillu220@gmail.com>
2026-04-14 20:51:54 +0530

    fix home redirect to tasks

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl text-red-500">
-        Tailwind Working 🚀
-      </h1>
-    </div>
-  );
+  redirect("/tasks");
 }
